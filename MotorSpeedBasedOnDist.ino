e73c930f06854dc3b4e89c486f9b4475a07f8b0f
amitdavidi <amitdavidi12@gmail.com>
2023-05-30 11:22:16 +0300

    added plots with python

--- MotorSpeedBasedOnDist.ino
+++ MotorSpeedBasedOnDist.ino
@@ -20,7 +20,6 @@ void setup(void)
   Wire.begin();
 
   Serial.begin(115200);
-  Serial.println("VL53L1X Qwiic Test");
 
   if (distanceSensor.begin() != 0) //Begin returns 0 on a good init
   {
@@ -30,9 +29,9 @@ void setup(void)
   }
 
   // init motor
-  pinMode(M0_PWM, OUTPUT);     // Set M0_PWM as an output pin
-  pinMode(M0_PHASE, OUTPUT);   // Set M0_PHASE as an output pin
-  digitalWrite(M0_PHASE, HIGH); // Set M0_PHASE to LOW to drive the motor in one direction
+  pinMode(M0_PWM, OUTPUT);     
+  pinMode(M0_PHASE, OUTPUT);  
+  digitalWrite(M0_PHASE, LOW); 
   pinMode(9, OUTPUT);
   pinMode(10, OUTPUT);
   pinMode(11, OUTPUT);
@@ -55,7 +54,7 @@ void loop(void)
   Serial.println(distance);
   
   int max_dist_val = min(distance, 1000);
-  motorSpeed = map(max_dist_val, 0, 1000, 255, 0);
+  motorSpeed = map(max_dist_val, 0, 1000, 0, 255);
   
   delay(5);
   // Set the motor speed based on the potentiometer value
