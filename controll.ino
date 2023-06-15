ce12c8c7e97c908040dbecba03dd4175a57efaba
Amit Davidi <65807063+AmitDavidi@users.noreply.github.com>
2023-06-15 18:15:49 +0300

    Update controll.ino

--- controll.ino
+++ controll.ino
@@ -8,14 +8,23 @@ Zumo32U4IMU imu;
 
 // time variables
 #define SAMPLERATE 10          // 10 millis =  100 Hz
+// Odometry settings
+#define GEAR_RATIO 51.45      // Motor gear ratio 100.37
+#define WHEELS_DISTANCE 98    // Distance between tracks in mm
+#define WHEEL_DIAMETER 37.5   // Wheels diameter measured 38.5
+#define ENCODER_PPR 12        // Encoder pulses per revolution
+#define GYRO_SCALE 0.07        // 70 mdps/LSB 
+
+
+
 unsigned long lastMillis = 0;
 unsigned long lastMicros = 0;
 float dt_time = SAMPLERATE/1000.0;
 
 // infinity points:
+float pointsX[40] =  {0.0, 0.07796279852044666, 0.15034585288772137, 0.21719158547807443, 0.27804329311496895, 0.331374919654008, 0.375175233291167, 0.40769678418515204, 0.4282388474624953, 0.4380465723780955, 0.43978844550784507, 0.43438963052241786, 0.42041136805942303, 0.39748811585457783, 0.3680960037173226, 0.335427069760104, 0.295518922240434, 0.23993788657416784, 0.17059485297182114, 0.09363333199658815, 0.01316676327986243, -0.06872081131573661, -0.1520287232859072, -0.23894690388736683, -0.32769558848069114, -0.4065089581103317, -0.46853728792986066, -0.5146048746259917, -0.5454010215198498, -0.5594005526637246, -0.5546623017199586, -0.532279264168877, -0.49575365042523034, -0.44780863260248954, -0.38982752768980655, -0.32269897112301027, -0.24753158656569446, -0.1663221409137514, -0.08244848233731378, -1.2246467991473532e-16};
+float pointsY[40] =  {-0.06, -0.06097065217830785, -0.0668716574196292, -0.0799390184802036, -0.09995710911601352, -0.1252915774822026, -0.1537273363718332, -0.1828552620066096, -0.21019745637179454, -0.2340088977241921, -0.2552460306252849, -0.27715975280112715, -0.3007143464819011, -0.3228008357055886, -0.3383103027430125, -0.3427959127754232, -0.330622709240164, -0.2853305277190237, -0.19135498708153148, -0.052461746692965126, 0.10717682367309084, 0.2547717012557486, 0.3670127106597232, 0.4364509036850294, 0.46254658791851405, 0.4519960315518679, 0.4195066532525018, 0.3750620654831845, 0.32272806491399, 0.2648867682386831, 0.20571098843630753, 0.15040120488758826, 0.10133233082784882, 0.05820566707903888, 0.02049037227055775, -0.011424533992903371, -0.03616216296944064, -0.052060080232308494, -0.05894547951670264, -0.06};
 
-//float pointsX[40] =  {0.0, 0.03208225617155205, 0.0633335987602945, 0.09294463440875371, 0.12014845284759577, 0.1442404894687629, 0.16459677317873128, 0.18069008692207644, 0.19210362232627445, 0.1985417748196108, 0.19983779963431395, 0.19595813040845356, 0.18700324853708297, 0.17320508075688776, 0.15492099236553092, 0.1326245316481591, 0.10689316522556025, 0.07839332197201504, 0.04786313285751162, 0.016093313743345217, -0.016093313743345078, -0.04786313285751149, -0.07839332197201498, -0.10689316522556014, -0.132624531648159, -0.15492099236553084, -0.17320508075688767, -0.18700324853708294, -0.19595813040845356, -0.19983779963431392, -0.19854177481961083, -0.1921036223262745, -0.1806900869220765, -0.16459677317873134, -0.14424048946876294, -0.12014845284759593, -0.09294463440875383, -0.06333359876029461, -0.03208225617155213, -4.898587196589413e-17};
-//float pointsY[40] =  {0.0, -0.0004050345323210414, -0.0029623154374641336, -0.008755371102152572, -0.017706515024622, -0.029156389950552055, -0.0423866006901744, -0.056837518566891655, -0.07211973225187653, -0.08794397723209986, -0.10402655977279358, -0.11997756354302339, -0.1351552823930171, -0.14846149779161807, -0.15806126685897606, -0.16106850013878748, -0.15341472258503872, -0.13046864767574942, -0.08922533766745615, -0.03192771407321133, 0.031927714073211065, 0.08922533766745591, 0.13046864767574934, 0.1534147225850387, 0.16106850013878746, 0.15806126685897612, 0.1484614977916181, 0.13515528239301716, 0.11997756354302341, 0.10402655977279361, 0.08794397723209996, 0.0721197322518766, 0.056837518566891704, 0.042386600690174435, 0.02915638995055208, 0.01770651502462206, 0.008755371102152607, 0.002962315437464144, 0.0004050345323210424, -0.0};
 
 // circle points
 //float pointsX[40] =  {0.15, 0.14805753939568692, 0.1422804662920718, 0.1328184038479815, 0.11991641451052518, 0.10390865302643992, 0.08520971200967338, 0.06430388421045813, 0.04173261958746791, 0.018080502038298484, -0.006039891016412235, -0.03000385406640665, -0.053190733056380314, -0.07499999999999997, -0.09486680633930657, -0.11227661222566512, -0.12677851283156918, -0.13799691654882362, -0.14564127261390777, -0.1495135962201315, -0.1495135962201315, -0.1456412726139078, -0.13799691654882362, -0.12677851283156924, -0.11227661222566519, -0.09486680633930665, -0.07500000000000007, -0.05319073305638038, -0.030003854066406682, -0.006039891016412271, 0.01808050203829835, 0.04173261958746781, 0.06430388421045806, 0.08520971200967334, 0.10390865302643988, 0.1199164145105251, 0.13281840384798144, 0.1422804662920718, 0.14805753939568692, 0.15};
@@ -25,12 +34,6 @@ float dt_time = SAMPLERATE/1000.0;
 //float pointsX[40] = {0.0, 0.05, 0.1, 0.15000000000000002, 0.2, 0.25, 0.30000000000000004, 0.35000000000000003, 0.4, 0.45, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.45, 0.4, 0.35000000000000003, 0.30000000000000004, 0.25, 0.2, 0.15000000000000002, 0.1, 0.05, 0.0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 //float pointsY[40] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.05, 0.1, 0.15000000000000002, 0.2, 0.25, 0.30000000000000004, 0.35000000000000003, 0.4, 0.45, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.45, 0.4, 0.35000000000000003, 0.30000000000000004, 0.25, 0.2, 0.15000000000000002, 0.1, 0.05};
 
-// Odometry settings
-#define GEAR_RATIO 51.45      // Motor gear ratio 100.37
-#define WHEELS_DISTANCE 98    // Distance between tracks in mm
-#define WHEEL_DIAMETER 37.5   // Wheels diameter measured 38.5
-#define ENCODER_PPR 12        // Encoder pulses per revolution
-#define GYRO_SCALE 0.07        // 70 mdps/LSB 
 
 float encoder2dist = (WHEEL_DIAMETER*3.14/(ENCODER_PPR*GEAR_RATIO)) / 1000.0;  // conversition of encoder pulses to distance in m
 
@@ -42,9 +45,7 @@ float gyroAngle=0;
 int32_t gyroOffset_z = -16;
 float gyroz=0;
 boolean motorsState = 0;
-
-float pointsX[40] = {0.0, 0.05, 0.1, 0.15000000000000002, 0.2, 0.25, 0.30000000000000004, 0.35000000000000003, 0.4, 0.45, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.45, 0.4, 0.35000000000000003, 0.30000000000000004, 0.25, 0.2, 0.15000000000000002, 0.1, 0.05, 0.0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-float pointsY[40] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.05, 0.1, 0.15000000000000002, 0.2, 0.25, 0.30000000000000004, 0.35000000000000003, 0.4, 0.45, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.45, 0.4, 0.35000000000000003, 0.30000000000000004, 0.25, 0.2, 0.15000000000000002, 0.1, 0.05};
+//
 
 
 //
@@ -66,8 +67,8 @@ float w_last = 0.0;
 float error_left = 0.0, error_right = 0.0;
 int switched = 0;
 
-float pidController(float error, float prevError, float &integral, float Kp) {
-  float Ki = 500.0 , Kd = 0.0, outMin = -400.0 , outMax = 400.0;
+float pidController(float error, float prevError, float &integral) {
+  float Kp = 1500, Ki = 450.0, Kd = 0.0, outMin = -400.0 , outMax = 400.0;
 
   float proportional = Kp * error;  // Calculate the proportional term
   integral += Ki * error * dt_time;  // Calculate the integral term
@@ -106,7 +107,7 @@ void P2P_CTRL(float X_Desired, float Y_Desired, float &Left_Motor, float &Right_
     
     // set limits on max speed command
 //    float WheelRadius = WHEEL_DIAMETER/2/1000;  // [m]
-    float w_Max = 0.4;  // max speed [m/s]
+    float w_Max = 0.5;  // max speed [m/s]
     
 
     if (w_forward > w_Max) {
@@ -116,12 +117,13 @@ void P2P_CTRL(float X_Desired, float Y_Desired, float &Left_Motor, float &Right_
     if(w_forward - w_last > 0.1) {
       w_forward = w_last + 0.1;
     }
+    
     w_last = w_forward;
 
-    Left_Motor = (float(w_forward - theta_t*0.25));
-    Right_Motor = float(w_forward + theta_t*0.25);
+    Left_Motor = float(w_forward - theta_t*0.35);
+    Right_Motor = float(w_forward + theta_t*0.35);
     
-    if(Left_Motor != Left_Motor) Left_Motor = 0.02;
+    if(Left_Motor != Left_Motor) Left_Motor = 0.02; // nan handling
     if(Right_Motor != Right_Motor) Right_Motor = 0.02;
       
 
@@ -183,24 +185,25 @@ void loop() {
 
   if (millis() - lastMillis >= SAMPLERATE){
 
-   
+    float dist2nextp = sqrt(pow(posx - desiredPosX, 2) + pow(posy - desiredPosY, 2));
     
-    if( sqrt(pow(posx - desiredPosX, 2) + pow(posy - desiredPosY, 2)) < 0.03 ) {
-      pointIdx = (pointIdx+1) % 40;
-      desiredPosX = pointsX[pointIdx];
-      desiredPosY = pointsY[pointIdx];
+    
+    if(  dist2nextp < 0.035 ) {
+      
+      if(sqrt(pow(posx - pointsX[pointIdx + 2], 2) + pow(posy - pointsY[pointIdx + 2], 2) < dist2nextp)) {
+        // skip next
+          pointIdx = (pointIdx+2) % 40; 
+          desiredPosX = pointsX[pointIdx];
+          desiredPosY = pointsY[pointIdx];
+      }
+    
+    else {
+          pointIdx = (pointIdx+1) % 40;
+          desiredPosX = pointsX[pointIdx];
+          desiredPosY = pointsY[pointIdx];
+      }
       
     }
-    
-
-
-    
-    
-    Serial.print(" desired = (");
-    Serial.print(desiredPosX);
-    Serial.print(", ");
-    Serial.print(desiredPosY);
-    Serial.print(")");
     
     lastMillis = millis();
 
@@ -211,15 +214,20 @@ void loop() {
     
    
    
-      
+     odometry();
     P2P_CTRL(desiredPosX, desiredPosY, LeftMotorSpeed_Cntrl_CMD, RightMotorSpeed_Cntrl_CMD);
-
-
  
-    odometry();
-
+   
     
 //    gyroIntegration();
+//
+//    LeftMotorSpeed_Cntrl_CMD = 0.3;
+//    RightMotorSpeed_Cntrl_CMD =0.3;
+//    Serial.print(" desired Speed = (");
+//    Serial.print(LeftMotorSpeed_Cntrl_CMD);
+//    Serial.print(", ");
+//    Serial.print(RightMotorSpeed_Cntrl_CMD);
+//    Serial.print(")");
     
     // calculate the error between <Wheel RadS = LeftMotorSpeed> and the output from P2P_CTRL = <LeftMotorSpeed_Cntrl_CMD>
     float errorLeft = LeftMotorSpeed_Cntrl_CMD - leftWheelSpeed;
@@ -231,45 +239,40 @@ void loop() {
 
     
     // insert error to pidController, insert the controll signal to the motors.
-    cEr_left = pidController(errorLeft, prevErrorLeft, integralLeft, 2000);
+    cEr_left = pidController(errorLeft, prevErrorLeft, integralLeft);
 
-    cEr_right = pidController(errorRight, prevErrorRight, integralRight, 2000); 
+    cEr_right = pidController(errorRight, prevErrorRight, integralRight); 
     
- 
+   
 
     motors.setLeftSpeed(cEr_left); // -400 --> 400
     motors.setRightSpeed(cEr_right);
 
-    
-    Serial.print(" desired = (");
-    Serial.print(desiredPosX);
-    Serial.print(", ");
-    Serial.print(desiredPosY);
-    Serial.print(")");
-    
-    Serial.print(" Pos = (");
-    Serial.print(cEr_left);
-    Serial.print(", ");
-    Serial.print(cEr_right);
-    Serial.println(")");
-    
-  
+        
+   
+//    
+//    Serial.print(" SpeedP2P = (");
+//    Serial.print(leftWheelSpeed);
+//    Serial.print(", ");
+//    Serial.print(rightWheelSpeed);
+//    Serial.print(")");
+//  
     prevErrorLeft = errorLeft;
     prevErrorRight = errorRight;
 
     
-    
-    Serial.print(" Pos = (");
-    Serial.print(posx);
-    Serial.print(", ");
-    Serial.print(posy);
-    Serial.println(")");
+//
+//    Serial.print(" Pos = (");
+//    Serial.print(posx);
+//    Serial.print(", ");
+//    Serial.print(posy);
+//    Serial.println(")");
     
     
   }
   
 
-    motorsState = (cEr_left || cEr_right) ==  0 ? 0 : 1; //  check if motors are still, for gyro
+//    motorsState = (cEr_left || cEr_right) ==  0 ? 0 : 1; //  check if motors are still, for gyro
 
 }
 
@@ -313,15 +316,18 @@ void odometry(){
     float d_theta = float(dx_1-dx_2)/(WHEELS_DISTANCE / 1000.0);
     posx += cos(theta+d_theta/2)*(dx_1+dx_2)/2;
     posy += sin(theta+d_theta/2)*(dx_1+dx_2)/2;
-    theta += d_theta;
+    
+    theta += d_theta*0.665*0.9; // band-aid
+   
+
 
     
 
     leftWheelSpeed = float(dx_2 / dt_time); 
     rightWheelSpeed = float(dx_1 / dt_time); 
-
-    leftWheelSpeed = LowPassFilter(leftWheelSpeed, lastleftWheelSpeed, 0.5);
-    rightWheelSpeed = LowPassFilter(rightWheelSpeed, lastrightWheelSpeed, 0.5);
+//
+    leftWheelSpeed = LowPassFilter(leftWheelSpeed, lastleftWheelSpeed, 0.2);
+    rightWheelSpeed = LowPassFilter(rightWheelSpeed, lastrightWheelSpeed, 0.2);
 
     lastrightWheelSpeed = rightWheelSpeed;
     lastleftWheelSpeed = leftWheelSpeed;
